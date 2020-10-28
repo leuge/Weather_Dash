1b7b6b37d53dd60fc8daeb169f7466bfe6149604
Windy <lauren.euge@gmail.com>
2020-10-27 23:29:11 -0700

    first attempt script.js

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+function getSearchVal() {
+    var searchValue = document.querySelector("#search-value").value;
+    searchWeather(searchValue);
+    makeRow(searchValue);
+}
+
+function makeRow(searchValue) {
+    var liEl=document.createElement("li")
+    liEl.classList.add("list-group-item", "list-group-item-action");
+    var text=searchValue;
+    liEl.textContent=text;
+    var historyEl=document.querySelector('.history');
+    console.log(event.target)
+    historyEl.onclick=function(){
+        console.log(event.target.tagName)
+        if (event.target.tagName == "LI"){
+            searchWeather(event.target.textContent)
+        }
+    }
+}
